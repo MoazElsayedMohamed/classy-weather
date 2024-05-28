90b87aabc8c5530557f114982a53827434aea3c3
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-05-28 17:49:26 +0300

    feat: :memo: create our constructor to set value to input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: "lisbon" };
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
         <div>
-          <input type="text" placeholder="Search for weather" />
+          <input
+            type="text"
+            placeholder="Search for weather"
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
         </div>
+        <button>Get Weather</button>
       </div>
     );
   }
