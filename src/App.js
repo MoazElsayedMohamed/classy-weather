0b3edda52e1ef169d234c22ae5a67fee7d4c4827
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-05-28 18:35:03 +0300

    feat: :sparkles: fetch weather and location data and set them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React from "react";
 
+function convertToFlag(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { location: "lisbon" };
+    this.state = {
+      location: "lisbon",
+      isLoading: false,
+      displaylocation: "",
+      weather: {},
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  async fetchWeather() {
+    try {
+      this.setState({ isLoading: true });
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error("Location not found");
+
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+      this.setState({
+        displaylocation: `${name} ${convertToFlag(country_code)}`,
+      });
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      this.setState({ weather: weatherData.daily });
+    } catch (err) {
+      console.err(err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
   }
 
   render() {
@@ -18,7 +63,9 @@ class App extends React.Component {
             onChange={(e) => this.setState({ location: e.target.value })}
           />
         </div>
-        <button>Get Weather</button>
+        <button onClick={this.fetchWeather}>Get Weather</button>
+
+        {(this, this.state.isLoading && <p className="loader">Loading</p>)}
       </div>
     );
   }
